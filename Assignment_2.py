03959891972bc18c0da8959e9ce888a762d0947b
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-05-09 21:52:12 +0530

    Update and rename Assignment_1.py to Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -38,7 +38,7 @@ deg=0
 y=0
 z=0
 for deg in range(0,346,15):
-rad= deg*(pi/180)
-y=round(sin(rad),4)
-z=round(cos(rad),4)
+  rad= deg*(pi/180)
+  y=round(sin(rad),4)
+  z=round(cos(rad),4)
 print("sin",deg,"=",y,"and cos",deg,"=",z)
