36f6550b134d1a125c91e163be414cd96e682b0f
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-05-25 17:00:02 +0530

    Add files via upload

--- Assignment_4.py
+++ Assignment_4.py
@@ -1,48 +1,42 @@
-#Ans1
-print(bin(int(input("Enter a Number: "))))
+#Ans1:
+grade = int(input("Enter grade: "))
+if(grade < 25): print('F')
+elif(grade in range(25, 45)): print('E')
+elif(grade in range(45, 50)): print('D')
+elif(grade in range(50, 60)): print('C')
+elif(grade in range(60, 80)): print('B')
+else: print('A')
 
-#Ans2
-print(eval(input("Enter the Expression: ")))
+#Ans2:
+def is_leap(year):
+    leap = False
+    if year%400==0:
+        leap= True
+    elif year%4==0 and year%100!=0:
+        leap=True
+    return leap
+year = int(input())
+print(is_leap(year))
 
-#Ans3
+#Ans3:
+import random
+for i in range(1, 11):
+    a = random.randint(0,10)
+    b = random.randint(0,10)
+    qstr = "Question " +str(i)+ ": " +str(a)+ " X " +str(b)+ " = "
+    ans = int(input(qstr))
+    if(ans != a*b):
+        print("Wrong. The answer is "+str(a*b))
+    else:
+        print("Right!")
 
-#a
-print((3+4)*(5))
-
-#b
-n = int(input("Enter n: "))
-print(n*(n-1)/2)
-
-#c
-from cmath import pi
-r = int(input("Enter Radius: "))
-print(4 * pi * r * r)
-
-#d
-import math
-r = int(input("Enter r: "))
-a = int(input("Enter a(radians): "))
-b = int(input("Enter b(radians): "))
-left = r * math.cos(a) * math.cos(a)
-right = r * math.sin(b) * math.sin(b)
-print(math.sqrt(left+right))
-
-#e
-x1 = int(input("Enter x1: "))
-x2 = int(input("Enter x2: "))
-y1 = int(input("Enter y1: "))
-y2 = int(input("Enter y2: "))
-print((y2-y1)/(x2-x1))
-
-# Ans4:
-# a) 0, 1, 2, 3, 4 
-# b) 3, 4, 5, 6, 7, 8, 9
-# c) 4, 7, 10
-# d) 15, 13, 11, 9, 7
-# e) no_solution
-
-#Ans5
-H = int(input("Enter number of Hydrogen atoms: "))
-C = int(input("Enter number of Carbon atoms: "))
-O = int(input("Enter number of Oxygen atoms: "))
-print("Molecular Weight: ", (H*1.00794 + C*12.0107 + O*15.9994))
+#Ans4:
+for candies in range(200):
+    if (candies % 5 != 2):
+        continue
+    if (candies % 6 != 3):
+        continue
+    if (candies % 7 != 2):
+        continue
+    print("There are a total " + str(candies) + " pieces in the bowl.")
+    break
